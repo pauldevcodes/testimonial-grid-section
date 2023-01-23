781f1eb191262453591bfd69eeabfcbf3d7c0b44
Paul Okwulu <pauldevcodes@gmail.com>
2023-01-23 23:08:00 +0100

    testimonials grid section complete

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      moderateViolet: 'hsl(263, 55%, 52%)',
+      veryDarkgrayishBlue: 'hsl(217, 19%, 35%)',
+      veryDarkBlackishBlue: 'hsl(219, 29%, 14%)',
+      white: 'hsl(0, 0%, 100%)',
+      lightGray: 'hsl(0, 0%, 81%)',
+      lightGrayishBlue: 'hsl(210, 46%, 95%)'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
